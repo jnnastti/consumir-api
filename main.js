7c4ba165693d91e9f68562ffdbaa95e9a2459399
jnnastti <janna.sangaletti6@gmail.com>
2021-10-18 14:09:23 -0300

    modalDialog

--- main.js
+++ main.js
@@ -31,15 +31,23 @@ function criarPersonagem(personagem) {
   let lista = document.getElementById('lista');
 
   // container sobre o personagem
-  let contPersonagem = document.createElement('div');
+  let contPersonagem = document.createElement('div')
+  
+  contPersonagem.addEventListener("click", () => {
+    pegarInfoPersonagem(personagem.id)
+
+    window.location.href = "#info"
+  });
+
+  contPersonagem.classList.add("item");
 
   let imagem = document.createElement('img');
   imagem.src = personagem.image;
   imagem.alt = personagem.name;
 
   contPersonagem.appendChild(imagem);
-  contPersonagem.classList.add("item");
 
+  // informações
   let divInfo = document.createElement('div');
 
   createAndAppend('h2', `${personagem.id} - ${personagem.name}`, divInfo)
@@ -62,5 +70,30 @@ function createAndAppend(tag, conteudo, pai) {
 
   return elemento
 }
+
+// pegar informações do personagem
+function pegarInfoPersonagem(id) {
+  let req = fetch(url + id)
+  let dado = req.then((response) => {
+    return response.json()
+  })
+
+  dado.then((dado) => {
+    setarCamposEditar(dado) 
+  })
+}
+
+function setarCamposEditar(dados) {
+  let id = document.getElementById("id")
+  let nome = document.getElementById("nome")
+  let status = document.getElementById("status")
+  let especie = document.getElementById("especie")
+  let imagem = document.getElementById("imagem")
+
+  // lista de episodios
+}
+
+//id, name, status, species e image
+
 
 listarPersonagem()
