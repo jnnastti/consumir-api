592936e33dbccf31736ef192f38bd9449a84e930
jnnastti <janna.sangaletti6@gmail.com>
2021-10-18 13:27:51 -0300

    listando personagens

--- main.js
+++ main.js
@@ -7,3 +7,60 @@ function calculaMargin() {
 }
 
 calculaMargin();
+
+//////////////////////
+
+const url = 'https://rickandmortyapi.com/api/character/';
+
+function listarPersonagem() {
+  let req = fetch(url)
+  let dado = req.then((response) => {
+    return response.json()
+  })
+     
+  dado.then((dado) => {
+    dado.results.forEach(element => {
+
+    // criar lista de personagens
+      criarPersonagem(element)
+    });
+  })
+}
+
+function criarPersonagem(personagem) {
+  let lista = document.getElementById('lista');
+
+  // container sobre o personagem
+  let contPersonagem = document.createElement('div');
+
+  let imagem = document.createElement('img');
+  imagem.src = personagem.image;
+  imagem.alt = personagem.name;
+
+  contPersonagem.appendChild(imagem);
+  contPersonagem.classList.add("item");
+
+  let divInfo = document.createElement('div');
+
+  createAndAppend('h2', `${personagem.id} - ${personagem.name}`, divInfo)
+  createAndAppend('h3', 'Status', divInfo)
+  createAndAppend('p', `${personagem.status}`, divInfo)
+  createAndAppend('h3', 'Especie', divInfo)
+  createAndAppend('p', `${personagem.species}`, divInfo)
+
+  contPersonagem.appendChild(divInfo);
+
+  lista.appendChild(contPersonagem)
+}
+
+// criar elemento filho e adicionar ao elemento pai
+function createAndAppend(tag, conteudo, pai) {
+  let elemento = document.createElement(tag);
+
+  elemento.textContent = conteudo
+  pai.appendChild(elemento)
+
+  return elemento
+}
+
+listarPersonagem()
